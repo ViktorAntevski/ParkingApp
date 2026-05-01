dfd00e2680e5e35bf51959948a1902240aca997b
Viktor <vantevski1@gmail.com>
2026-05-01 22:01:25 +0200

    Commit up to Sign-up--> switch sign-up from form submission to JSON via JavaScript. defined tables. ordered code structure

--- parkingapp/Models/models.py
+++ parkingapp/Models/models.py
@@ -1,48 +1,73 @@
 from parkingapp import db
+from flask_login import UserMixin
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+Skopje_TZ = ZoneInfo("Europe/Skopje")
 
 
-#base class: RegistrationPlates, operator queries here
-class RegistrationPlates(db.Model):
-    __tablename__ = "current_users"
+class User(db.Model, UserMixin):
+    __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), unique=True, nullable=False)
+    name_surname = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(50), nullable=False)
+    email_address = db.Column(db.String(50), nullable=False)
+    ID_card = db.Column(db.String(20), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<{User}: {self.username}>'
+
+
+#base class: Registration Plates, Parking Operator queries here, these are the common attributes of all types of services,
+# if not in table -> car should be fined
+class ActiveRegistrationPlate(db.Model):
+    __tablename__ = "active_users"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(db.Integer, db.ForeignKey("User.id"))
     vehicle_reg_plate = db.Column(db.String(20), unique=True, nullable=False)
     registered_parking_zone = db.Column(db.String(5), nullable=False)
     fined = db.Column(db.Boolean, default=False)
 
-#subtype tables
+#ShortTerm  metering - which user, how much hours should be billed
 class ShortTermParking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reg_plates_id = db.Column(db.Integer, foreign_key="current_users.id")
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    check_in = db.Column(db.Datetime(timezone=True), default=lambda: datetime.now(Skopje_TZ), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("User.id"))
+    check_in = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(Skopje_TZ), nullable=False)
+    provider = db.Column(db.String(50), nullable=False)
 
-class MonthlySubs(db.Model):
+
+# Monthly subs - once a user is written in this table, only payed_for_month=True/False is updated monthly
+# Schedule: when payed_for_month = True (at the beginning of the month), remove the user from ActiveRegistrationPlates and
+# set to False
+class MonthlySub(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reg_plates_id = db.Column(db.Integer, foreign_key="current_users.id")
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    address = db.Column(db.String(100), nullable=False)
-    name_surname = db.Column(db.String(50), nullable=False)
-    ID_card = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("User.id"))
     payed_for_month = db.Column(db.Boolean, default = False)
+    last_updated = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(Skopje_TZ), onupdate=lambda: datetime.now(Skopje_TZ), nullable=False)
 
-class Residents(db.Model):
+#This table has no function in the REST API, only for analytics
+class Resident(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reg_plates_id = db.Column(db.Integer, foreign_key="current_users.id")
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    address = db.Column(db.String(100), nullable=False)
-    name_surname = db.Column(db.String(50), nullable=False)
-    ID_card = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("User.id"))
+    registration_time = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(Skopje_TZ), nullable=False)
 
-#might be obsolete, merge with base, check if any business logic differences exist between sms users and guests
-class Guests(db.Model):
+##might be obsolete, merge with base, check if any business logic differences exist between sms users and guests
+class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     reg_plates_id = db.Column(db.Integer, unique=True, nullable=False)
 
-class Operators(db.Model):
+class Operator(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name_surname = db.Column(db.String(50), nullable=False)
-    ID_card = db.Column(db.String(20), nullable=False)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
-class GuestCards(db.Model):
+#class GuestCard(db.Model):
