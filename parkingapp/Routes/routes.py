193c307076ad88ef6f5706e0c9efaf61d1464ae8
Viktor <vantevski1@gmail.com>
2026-05-01 13:10:57 +0200

    New project structure and gitignore commited to exclude folders which shouldn't be commited to Git

--- parkingapp/Routes/routes.py
+++ parkingapp/Routes/routes.py
@@ -1,11 +1,13 @@
+from flask import Flask, url_for, redirect
 from flask_restful import Resource,Api, marshal_with, fields, reqparse, abort
+
 
 api = Api(App)
 
-add_sms_users = reqparse.RequestParser()
-add_sms_Users.add_argument()
-add_sms_Users.add_argument()
-add_sms_Users.add_argument()
+add_shortterm_users = reqparse.RequestParser()
+add_shortterm_Users.add_argument("phone_number", type=str, help="")
+add_shortterm_Users.add_argument()
+add_shortterm_Users.add_argument()
 
 
 add_residents = reqparse.RequestParser()
@@ -14,7 +16,7 @@ add_residents.add_argument()
 add_residents.add_argument()
 
 
-class SmsUsers(Resource):
+class ShortTermParking(Resource):
     def post(self):
 
 
@@ -24,3 +26,6 @@ class MonthlySubs(Resource):
 
 
 class Fined(Resource):
+
+
+api.add_resource(SmsUsers, )
