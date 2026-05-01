41a457498c8226fccaaf703383687240bdaeca1d
Viktor <YOUR_NOREPLY_EMAIL>
2026-05-01 13:10:57 +0200

    New project structure and gitignore commited to exclude folders which shouldn't be commited to Git

--- parkingapp/Models/models.py
+++ parkingapp/Models/models.py
@@ -1,24 +1,25 @@
-from parkingapp.parkingapp import db
-from flask_sqlalchemy import SQLAlchemy
+from parkingapp import db
+
 
 #base class: RegistrationPlates, operator queries here
 class RegistrationPlates(db.Model):
     __tablename__ = "current_users"
     id = db.Column(db.Integer, primary_key=True)
     vehicle_reg_plate = db.Column(db.String(20), unique=True, nullable=False)
-    phone_number = db.Column(db.String(20),unique=True, nullable=False)
     registered_parking_zone = db.Column(db.String(5), nullable=False)
     fined = db.Column(db.Boolean, default=False)
 
 #subtype tables
-class SmsUsers(db.Model):
+class ShortTermParking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     reg_plates_id = db.Column(db.Integer, foreign_key="current_users.id")
+    username = db.Column(db.String(20), unique=True, nullable=False)
     check_in = db.Column(db.Datetime(timezone=True), default=lambda: datetime.now(Skopje_TZ), nullable=False)
 
 class MonthlySubs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     reg_plates_id = db.Column(db.Integer, foreign_key="current_users.id")
+    username = db.Column(db.String(20), unique=True, nullable=False)
     address = db.Column(db.String(100), nullable=False)
     name_surname = db.Column(db.String(50), nullable=False)
     ID_card = db.Column(db.String(20), nullable=False)
@@ -27,13 +28,21 @@ class MonthlySubs(db.Model):
 class Residents(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     reg_plates_id = db.Column(db.Integer, foreign_key="current_users.id")
+    username = db.Column(db.String(20), unique=True, nullable=False)
     address = db.Column(db.String(100), nullable=False)
     name_surname = db.Column(db.String(50), nullable=False)
     ID_card = db.Column(db.String(20), nullable=False)
 
+#might be obsolete, merge with base, check if any business logic differences exist between sms users and guests
 class Guests(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reg_plates_id = db.Column(db.Integer, foreign_key="current_users.id")
+    reg_plates_id = db.Column(db.Integer, unique=True, nullable=False)
 
+class Operators(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name_surname = db.Column(db.String(50), nullable=False)
+    ID_card = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
-#class GuestCards(db.Model):
+class GuestCards(db.Model):
