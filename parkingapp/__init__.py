86ddae7363b388fce19d237235e405a34ff63969
Viktor <vantevski1@gmail.com>
2026-05-11 23:49:17 +0200

    Grouped routes into blueprints. Now, authentication required links concerning dashboard go under the blueprint "dashboard", while all http pages prior to login go to "pages".

--- parkingapp/__init__.py
+++ parkingapp/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from parkingapp.mail_config import Config
+from parkingapp.extensions import mail
 
 
 db = SQLAlchemy()
@@ -15,10 +17,12 @@ def create_app():
     app = Flask(__name__, template_folder = "templates")
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parking_data.db'
     app.config["SECRET_KEY"] = "SOME KEY"
+    app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
+    mail.init_app(app)
     migrate.init_app(app, db)
 
     from parkingapp.Models.models import User
@@ -28,7 +32,9 @@ def create_app():
         return User.query.get(int(user_id))
 
     from parkingapp.Routes.pages import pages
+    from parkingapp.Routes.dashboard import dashboard
     app.register_blueprint(pages)
+    app.register_blueprint(dashboard)
 
     from parkingapp.Routes.rest_routes import api_bp
     app.register_blueprint(api_bp)
