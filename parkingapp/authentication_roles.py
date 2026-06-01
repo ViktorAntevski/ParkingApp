71655cff156ade23f2985b24286c6c70fcee0421
Viktor <YOUR_NOREPLY_EMAIL>
2026-06-02 00:41:22 +0200

    Started to work on the Operators domain. This incorporates a separate Login end point(query user table vs query operator table) and redirect to the appropriate dashboard. now, all endpoints related to operator are decorated with operator_required and all user end points are decorated with user_required wrapper function. Since users and operators are from a different table, ID's may overlap in unexpected ways. To overcome this at the system level, I override the get_id method from UserMixin of flask-login. now it is a pair "user:1" or "operator:1" and the load_user function of login_manager has extended logic so it can separate between the two groups.

---
+++ parkingapp/authentication_roles.py
@@ -0,0 +1,64 @@
+from functools import wraps
+from flask_login import current_user
+from flask import abort
+from parkingapp.Models.models import User, Operator
+from flask_login import LoginManager
+
+
+def register_user_loader(login_manager):
+    @login_manager.user_loader
+    def load_user(user_id):
+        try:
+            type_, id_ = user_id.split(":")
+            id_ = int(id_)
+        except (ValueError, AttributeError):
+            return None
+
+        if type_ == "user":
+            user = User.query.get(id_)
+            if user:
+                user.user_type = "user"
+            return user
+
+        elif type_ == "operator":
+            operator = Operator.query.get(id_)
+            if operator:
+                operator.user_type = "operator"
+            return operator
+
+        return None
+
+
+
+
+
+
+
+##### ROLE SEPARATION######
+def user_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated:
+            abort(401)
+
+        if not current_user.get_id().startswith("user:"):
+            abort(403)
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def operator_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated:
+            abort(401)
+
+        if not current_user.get_id().startswith("operator:"):
+            abort(403)
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
