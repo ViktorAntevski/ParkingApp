370765719ad203acd249b09c1e884c1889374978
Viktor <YOUR_NOREPLY_EMAIL>
2026-06-10 01:04:10 +0200

    SECURITY: moved mail config information into production server .env 2

--- parkingapp/config.py
+++ parkingapp/config.py
@@ -1,8 +1,10 @@
 import os
 
-MAIL_SERVER = os.getenv("MAIL_SERVER")
-MAIL_PORT = int(os.getenv("MAIL_PORT",587))
-MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "True"
-MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+class MailConfig:
+
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = int(os.getenv("MAIL_PORT",587))
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "True"
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
