6363f47ac1418ac8593365da4fd346c2bb0b228b
Viktor <vantevski1@gmail.com>
2026-06-10 00:51:50 +0200

    SECURITY: moved mail config information into production server .env

--- parkingapp/config.py
+++ parkingapp/config.py
@@ -1,8 +1,8 @@
+import os
 
-class MailConfig:
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = "vantevski1@gmail.com"
-    MAIL_PASSWORD = "ilwmptdnbsxzinqg"
-    MAIL_DEFAULT_SENDER = "vantevski1@gmail.com"
+MAIL_SERVER = os.getenv("MAIL_SERVER")
+MAIL_PORT = int(os.getenv("MAIL_PORT",587))
+MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "True"
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
